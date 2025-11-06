14b612600901f738b71d4bc208139712109e73cd
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2025-11-07 01:21:46 +0330

    completed gread exercise

---
+++ P11-array/scripts/app.js
@@ -0,0 +1,7 @@
+let gread = [15,17,16,20,20,14,19,13,20]
+var sum = 0;
+for(var i = 0; i < gread.length; i++){
+    sum += gread[i]
+    var average = sum / gread.length
+}
+console.log("your gread is : ", average);
