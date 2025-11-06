29effc05c10425f47c6a58748340bdc086f4371e
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2025-11-06 20:18:13 +0330

    completed P-3

--- P10-nestedLoop/scripts/app.js
+++ P10-nestedLoop/scripts/app.js
@@ -9,17 +9,32 @@
 // }
 // console.log(star);
 //P-2
+// var star = "";
+// for(var i = 1; i < 6; i++){
+//     for(var j = 0; j < i; j++){
+//         star += "*";
+//         star +="\t"
+//     }        
+//     star += "\n";
+// }
+// console.log(star);
+// *	
+// *	*	
+// *	*	*	
+// *	*	*	*	
+// *	*	*	*	*	
+//P-3
 var star = "";
 for(var i = 1; i < 6; i++){
-    for(var j = 0; j < i; j++){
+    for(var j = i; j < 6; j++){
         star += "*";
         star +="\t"
     }        
     star += "\n";
 }
 console.log(star);
-// *	
-// *	*	
-// *	*	*	
-// *	*	*	*	
 // *	*	*	*	*	
+// *	*	*	*	
+// *	*	*	
+// *	*	
+// *	
