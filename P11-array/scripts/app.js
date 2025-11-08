67fb7ed3fc8f70af6e252e43d68cd39cdb9d9f66
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2025-11-08 14:04:28 +0330

    Completion of the second exercise

--- P11-array/scripts/app.js
+++ P11-array/scripts/app.js
@@ -1,7 +1,18 @@
-let gread = [15,17,16,20,20,14,19,13,20]
-var sum = 0;
-for(var i = 0; i < gread.length; i++){
-    sum += gread[i]
-    var average = sum / gread.length
+// let gread = [15,17,16,20,20,14,19,13,20]
+// var sum = 0;
+// for(var i = 0; i < gread.length; i++){
+//     sum += gread[i]
+//     var average = sum / gread.length
+// }
+// console.log("your gread is : ", average);
+
+// The largest number in the array ==> The second exercise
+
+let numArray = [12,15,68,23,20,102,36,83,41,78]
+let result = numArray[0];
+for (var i = 0; i < numArray.length; i++){
+    if(result <= numArray[i]){
+        result = numArray[i]
+    }
 }
-console.log("your gread is : ", average);
+console.log(result);
