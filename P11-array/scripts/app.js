116c7bdc7bf8c621e28ae9fdc62d06ebca6f7240
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2025-11-09 13:46:24 +0330

    Completion remove duplicate values exercise

--- P11-array/scripts/app.js
+++ P11-array/scripts/app.js
@@ -32,10 +32,21 @@
 
 // Reverse the numbers ==> The fourth exercise
 
-let numArray = [10, 15, 20, 25, 30, 35, 40];
-let reverseArray = [];
+// let numArray = [10, 15, 20, 25, 30, 35, 40];
+// let reverseArray = [];
 
-for(var i = numArray.length; i >= 0; i--){
-    reverseArray.push(numArray[i])
+// for(var i = numArray.length; i >= 0; i--){
+//     reverseArray.push(numArray[i])
+// }
+// console.log(reverseArray);
+
+//Remove duplicate values ==> The fifth exercise
+let numArray = [83,90,97,81,83,85,90,78,79,85];
+let number = [];
+for(var i = 0; i < numArray.length; i++){
+    
+    if(number.includes(numArray[i]) == false){
+        number.push(numArray[i])
+    }
 }
-console.log(reverseArray);
+console.log(number);
