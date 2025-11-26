c1b38aab1c1a8b0a768176285e8715253e7ed6b8
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2025-11-26 12:53:11 +0330

    forEach

---
+++ P14-method/scripts/app.js
@@ -0,0 +1,10 @@
+myAraay = [12,67,35,25,96]
+//use forEach
+myAraay.forEach(element => {
+    console.log(element);
+});
+myAraay.forEach(isOdd => {
+    if(isOdd % 2 != 0){
+        console.log(isOdd);
+    }
+})
