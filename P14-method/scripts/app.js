b10c2bafa2cadcea977fa47f7e6e836090b02c98
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2025-11-26 13:03:41 +0330

    compeleted exercise 2

--- P14-method/scripts/app.js
+++ P14-method/scripts/app.js
@@ -1,10 +1,19 @@
-myAraay = [12,67,35,25,96]
+let myAraay = [12,67,35,25,96]
 //use forEach
 myAraay.forEach(element => {
     console.log(element);
 });
+//exercise 1
+console.log("---------------");
 myAraay.forEach(isOdd => {
     if(isOdd % 2 != 0){
         console.log(isOdd);
     }
-})
+});
+// exercise 2
+console.log("---------------");
+let dubleMyArray = [];
+myAraay.forEach(duble => {
+    dubleMyArray.push(duble * 5);
+});
+dubleMyArray.forEach(item => console.log("duble numers => ", item));
