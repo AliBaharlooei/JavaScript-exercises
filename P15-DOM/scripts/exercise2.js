494fe91364e896b8ec042a0bec35bd9356b52abc
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2025-12-05 17:32:50 +0330

    compeleted exercise2

--- P15-DOM/scripts/exercise2.js
+++ P15-DOM/scripts/exercise2.js
@@ -6,9 +6,19 @@ innerBtn.onclick = () => {
     console.log(box.getAttribute("id"));
     box.removeAttribute("id")
     box.setAttribute("id", "change-text__blue");
+
+    box.style.width = "250px"
+    box.style.height = "250px"
+    box.style.marginTop = "20px"
+    box.style.borderRadius = "12px"
 }
 outerBtn.onclick = () => {
     console.log(box.getAttribute("id"));
     box.removeAttribute("id");
     box.setAttribute("id", "change-text__box-green");
+
+    box.style.width = "100px"
+    box.style.height = "100px"
+    box.style.marginTop = "20px"
+    box.style.borderRadius = "12px"
 }
