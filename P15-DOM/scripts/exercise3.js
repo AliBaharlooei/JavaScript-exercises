c12bf093402d7c7862b260bb96b6f3dadf9ff99c
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2025-12-05 19:19:20 +0330

    compeleted update create random color exercise3

--- P15-DOM/scripts/exercise3.js
+++ P15-DOM/scripts/exercise3.js
@@ -4,8 +4,11 @@ const boxRandomColor = document.getElementById("box-random-color");
 btnRandomColor.onclick = () => {
     let numbersArray = [];
     for (i = 0; i < 3; i++){
-        let createColor = Math.random()*1000;
+        let createColor = Math.random()*1000/4;
         numbersArray.push(Math.round(createColor));
     }
     boxRandomColor.style.backgroundColor = `rgb(${numbersArray[0]}, ${numbersArray[1]}, ${numbersArray[2]})`;
+    let x = getComputedStyle(boxRandomColor);
+    console.log(x.backgroundColor);
+    
 }
