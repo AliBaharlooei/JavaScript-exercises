50bba3e9e7a6eacd90a3ca369a18fd1fbe4892a7
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2025-12-05 18:06:22 +0330

    compeleted exercise3

---
+++ P15-DOM/scripts/exercise3.js
@@ -0,0 +1,11 @@
+const btnRandomColor = document.getElementById("change-color");
+const boxRandomColor = document.getElementById("box-random-color");
+
+btnRandomColor.onclick = () => {
+    let numbersArray = [];
+    for (i = 0; i < 3; i++){
+        let createColor = Math.random()*1000;
+        numbersArray.push(Math.round(createColor));
+    }
+    boxRandomColor.style.backgroundColor = `rgb(${numbersArray[0]}, ${numbersArray[1]}, ${numbersArray[2]})`;
+}
