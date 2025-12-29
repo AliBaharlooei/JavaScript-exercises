52094f9d7f825b152e2464ef6f2ea71f042a1ed6
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2025-12-29 12:25:13 +0330

    Completion of keyboard practice

--- P17-evintKeyboard/scripts/keyboard.js
+++ P17-evintKeyboard/scripts/keyboard.js
@@ -1,3 +1,13 @@
+const btns = document.querySelectorAll('button');
 document.addEventListener('keydown', (event) => {
-    console.log(event.code);
-})
+    btns.forEach(element => {
+        if(element.id == event.code)
+            element.classList.add("down");              
+    });
+});
+document.addEventListener('keyup', event => {
+    btns.forEach(element => {
+        if(element.id == event.code)
+            element.classList.remove("down");              
+    });
+});
