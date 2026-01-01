de825266cdfb5f44d40c1c14c351a35e5c832bef
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2026-01-01 17:32:41 +0330

    this exercise have a bug but compeleted event flow

--- P18-exercise/scripts/app.js
+++ P18-exercise/scripts/app.js
@@ -1,8 +1,14 @@
-const accordion = document.querySelectorAll(".box-title")
+const accordion = document.querySelector(".container")
 const panle = document.querySelectorAll(".box-info")
-accordion.forEach(btn => {
-    btn.addEventListener('click', event => {
-        const text = btn.nextElementSibling;
-        text.classList.toggle("open")
-    })
-});
+// v1
+// accordion.forEach(btn => {
+//     btn.addEventListener('click', event => {
+//         const text = btn.nextElementSibling;
+//         text.classList.toggle("open")
+//     })
+// });
+// v2
+accordion.addEventListener('click', event => {
+    const elment = event.target.nextElementSibling;
+    elment.classList.toggle("open")  
+})
