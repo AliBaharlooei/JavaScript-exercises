063f66e12089fde528276c43f06a8101da664b6a
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2026-02-03 12:28:36 +0330

    compeleted exercise7

--- P22-BOM/scripts/exercise7.js
+++ P22-BOM/scripts/exercise7.js
@@ -1,9 +1,23 @@
 let textTimer = document.getElementById("timer");
-let stop;
-const intervalId = setInterval(()=>{
-    stop = textTimer.innerHTML = Number(textTimer.innerHTML) - 1;
-    if(stop == 0){
-        textTimer.innerHTML = "زمان به پایان رسید"
-        clearInterval(intervalId)
-    };
-},1000);
+let startBtn = document.getElementById("startBtn");
+let endBtn = document.getElementById("endBtn");
+let stop, intervalId;
+
+startBtn.addEventListener('click', ()=> {
+    startBtn.disabled = true;
+    endBtn.disabled= false;
+    intervalId = setInterval(()=>{
+        stop = textTimer.innerHTML = Number(textTimer.innerHTML) - 1;
+        if(stop == 0){
+            textTimer.innerHTML = "زمان به پایان رسید";
+            clearInterval(intervalId);
+            startBtn.disabled = true;
+            endBtn.disabled = true;
+        };
+    },1000);
+});
+endBtn.addEventListener('click', ()=> {
+    startBtn.disabled = false;
+    endBtn.disabled = true;
+    clearInterval(intervalId)
+});
