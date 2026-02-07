2a0a718725189ea5b7014a2df1b4a7219ef1de8a
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2026-02-07 14:06:58 +0330

    compeleted exercise2

---
+++ P25-DOM traversing/scripts/exercise2.js
@@ -0,0 +1,9 @@
+let c1 = document.createElement('h1');
+let c1Text = document.createTextNode('Hi im h1');
+c1.append(c1Text);
+document.querySelector('.container1').append(c1);
+
+let p1 = document.createElement('p');
+let p1Text = document.createTextNode('hi Ali Baharlooei');
+p1.append(p1Text);
+document.querySelector('.container2').append(p1)
