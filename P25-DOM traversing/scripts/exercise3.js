27a0949e06e13c7b63984c8e36dff1129ca1d63a
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2026-02-08 15:26:45 +0330

    compeleted exercise3-2

--- P25-DOM traversing/scripts/exercise3.js
+++ P25-DOM traversing/scripts/exercise3.js
@@ -8,3 +8,10 @@ let myName = createEl('p','Im Ali Baharlooei last El');
 container.appendChild(myName);
 let myAge = createEl('p','Im 21 years old');
 container.insertBefore(myAge,container.children[1]);
+
+// let item2 = container.children[2].firstElementChild.nextElementSibling;
+// let removed = container.children[2].removeChild(item2)
+// console.log("removed item" , removed);
+
+// let textAge = container.childNodes[1]
+// container.replaceChild(myName, textAge)
