0d513469ac586d9a641146050ee274c65aef5209
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2026-02-08 12:08:51 +0330

    compeleted exercise3-1

--- P25-DOM traversing/scripts/exercise3.js
+++ P25-DOM traversing/scripts/exercise3.js
@@ -1,14 +1,10 @@
-let container = document.getElementsByClassName("container");
+let container = document.getElementById("container");
 let createEl = (tagName,tagContent)=>{
-    //=> 1
-    // let tg = document.createElement(tagName);
-    // let tc = document.createTextNode(tagContent);
-    // tg.append(tc);
-    // container[0].append(tg);
-    //=> 2
     let tag = document.createElement(tagName);
     tag.textContent = tagContent
-    container[0].append(tag)
+    return tag
 }
-createEl('h1','hello world');
-createEl('p','Im Ali Baharlooei');
+let myName = createEl('p','Im Ali Baharlooei last El');
+container.appendChild(myName);
+let myAge = createEl('p','Im 21 years old');
+container.insertBefore(myAge,container.children[1]);
