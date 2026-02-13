616580cf64f56e375fb5945aa7de795239782908
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2026-02-13 14:26:51 +0330

    compeleted the add task btn

---
+++ P26-todo list/scripts/app.js
@@ -0,0 +1,47 @@
+let btnAddTask = document.getElementById("btn_add-task");
+let containerTodo = document.querySelector(".container-todo");
+
+//=> function for add task
+let addTask = () => {
+    let taskTitle = prompt("تسک مد نظر خود را وارد کنید");
+    let taskDate = prompt("تاریخ سر رسید را وارد کنید","1402/02/17");
+    // create box
+    const box = document.createElement('div');
+    box.classList.add("container-todo__box");
+    // container todo box check
+    const boxCheck = document.createElement('div');
+    boxCheck.classList.add("container-todo__box-check");
+    const todoCheck = document.createElement('input');
+    todoCheck.name = "todoCheck";
+    todoCheck.type = "checkbox";
+    boxCheck.appendChild(todoCheck);
+    box.appendChild(boxCheck);
+    // container todo box text
+    const boxTitle = document.createElement('div');
+    boxCheck.classList.add("container-todo__box-title");
+    const titleTop = document.createElement('h3');
+    titleTop.classList.add("container-todo__box-title__top");
+    titleTop.textContent = taskTitle;
+    const titleBottom = document.createElement('p');
+    titleBottom.classList.add("container-todo__box-title__text__bottom");
+    titleBottom.textContent = `تاریخ سر رسید ${taskDate}`;
+    boxTitle.appendChild(titleTop);
+    boxTitle.appendChild(titleBottom);
+    box.appendChild(boxTitle);
+    // container todo box action
+    const boxAction = document.createElement('div');
+    boxCheck.classList.add("container-todo__box-action");
+    const imgDelete = document.createElement('img');
+    imgDelete.src = "./imgs/icons8-bin-48.png";
+    imgDelete.classList.add("container-todo__box-action__delete");
+    const imgEdit = document.createElement('img');
+    imgEdit.src = "./imgs/icons8-eye-48.png";
+    imgEdit.classList.add("container-todo__box-action__edit");
+    boxAction.appendChild(imgDelete);
+    boxAction.appendChild(imgEdit);
+    box.appendChild(boxAction);
+    // append to box
+    containerTodo.appendChild(box);
+};
+
+btnAddTask.addEventListener('click', addTask);
