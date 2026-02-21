96ca4ffd4711da572ea20b69e2939298c2716483
Ali Baharlooei <ali.baharlooei.dev@gmail.com>
2026-02-21 17:09:39 +0330

    edit html code and add localStorage for title

--- P26-todo list/scripts/app.js
+++ P26-todo list/scripts/app.js
@@ -1,50 +1,102 @@
 let btnAddTask = document.getElementById("btn_add-task");
 let containerTodo = document.querySelector(".container-todo");
+let titleArray, dateArray;
+
+//=> check key value in local storage not null
+let checkEmptyArray = ()=>{    
+    if(!(localStorage.getItem("title") && localStorage.getItem("date"))){
+        titleArray = [];
+        dateArray = [];
+    }
+    else{
+        titleArray = localStorage.getItem("title").split(',');
+        dateArray = localStorage.getItem("date").split(',');
+    }
+}
+checkEmptyArray();
+
+//=> save task in the local storage
+let saveTask = (title,date)=>{
+    titleArray.push(title)
+    localStorage.setItem("title",titleArray);
+    dateArray.push(date);
+    localStorage.setItem("date",dateArray);
+}
+let showTitleValue = ()=>{    
+    let lastTaskValue;
+    if(localStorage.getItem("title")){
+        let getTaskArray = localStorage.getItem("title").split(",");
+        let lastTaskArray = getTaskArray.length - 1;
+        lastTaskValue = getTaskArray[lastTaskArray]
+    }
+    return lastTaskValue
+}
+console.log(showTitleValue());
+
+let showDateValue = ()=>{    
+    let lastDateValue;
+    if(localStorage.getItem("date")){
+        let getDateArray = localStorage.getItem("date").split(",");
+        let lastDateArray = getDateArray.length - 1;
+        lastDateValue = getDateArray[lastDateArray]
+    }
+    return lastDateValue
+}
+console.log(showDateValue());
+
+//=> show task
+let showTask = ()=>{
+    for (const element of titleArray) {
+        // create box
+        const box = document.createElement('div');
+        box.classList.add("container-todo__box");
+        // container todo box check
+        const boxCheck = document.createElement('div');
+        boxCheck.classList.add("container-todo__box-check");
+        const todoCheck = document.createElement('input');
+        todoCheck.name = "todoCheck";
+        todoCheck.type = "checkbox";
+        boxCheck.appendChild(todoCheck);
+        box.appendChild(boxCheck);
+        // container todo box text
+        const boxTitle = document.createElement('div');
+        boxTitle.classList.add("container-todo__box-title");
+        const titleTop = document.createElement('h3');
+        titleTop.classList.add("container-todo__box-title__top");
+        titleTop.textContent = element;
+        const titleBottom = document.createElement('p');
+        titleBottom.classList.add("container-todo__box-title__text__bottom");
+        titleBottom.textContent = `تاریخ سر رسید ${showDateValue()}`;
+        boxTitle.appendChild(titleTop);
+        boxTitle.appendChild(titleBottom);
+        box.appendChild(boxTitle);
+        // container todo box action
+        const boxAction = document.createElement('div');
+        boxAction.classList.add("container-todo__box-action");
+        const imgDelete = document.createElement('img');
+        imgDelete.src = "./imgs/icons8-bin-48.png";
+        imgDelete.classList.add("container-todo__box-action__delete");
+        const imgEdit = document.createElement('img');
+        imgEdit.src = "./imgs/icons8-eye-48.png";
+        imgEdit.classList.add("container-todo__box-action__edit");
+        boxAction.appendChild(imgDelete);
+        boxAction.appendChild(imgEdit);
+        box.appendChild(boxAction);
+        // append to box
+        containerTodo.appendChild(box);
+    }
+}
+showTask()
 
 //=> function for add task
 let addTask = () => {
     let taskTitle = prompt("تسک مد نظر خود را وارد کنید");
     let taskDate = prompt("تاریخ سر رسید را وارد کنید","1402/02/17");
-    // create box
-    const box = document.createElement('div');
-    box.classList.add("container-todo__box");
-    // container todo box check
-    const boxCheck = document.createElement('div');
-    boxCheck.classList.add("container-todo__box-check");
-    const todoCheck = document.createElement('input');
-    todoCheck.name = "todoCheck";
-    todoCheck.type = "checkbox";
-    boxCheck.appendChild(todoCheck);
-    box.appendChild(boxCheck);
-    // container todo box text
-    const boxTitle = document.createElement('div');
-    boxTitle.classList.add("container-todo__box-title");
-    const titleTop = document.createElement('h3');
-    titleTop.classList.add("container-todo__box-title__top");
-    titleTop.textContent = taskTitle;
-    const titleBottom = document.createElement('p');
-    titleBottom.classList.add("container-todo__box-title__text__bottom");
-    titleBottom.textContent = `تاریخ سر رسید ${taskDate}`;
-    boxTitle.appendChild(titleTop);
-    boxTitle.appendChild(titleBottom);
-    box.appendChild(boxTitle);
-    // container todo box action
-    const boxAction = document.createElement('div');
-    boxAction.classList.add("container-todo__box-action");
-    const imgDelete = document.createElement('img');
-    imgDelete.src = "./imgs/icons8-bin-48.png";
-    imgDelete.classList.add("container-todo__box-action__delete");
-    const imgEdit = document.createElement('img');
-    imgEdit.src = "./imgs/icons8-eye-48.png";
-    imgEdit.classList.add("container-todo__box-action__edit");
-    boxAction.appendChild(imgDelete);
-    boxAction.appendChild(imgEdit);
-    box.appendChild(boxAction);
-    // append to box
-    containerTodo.appendChild(box);
+    saveTask(taskTitle,taskDate);
 };
 //=> btn add task
 btnAddTask.addEventListener('click', addTask);
+
 //=> btn completion task
 containerTodo.addEventListener('click',(element)=>{
     let isCkeckBoxClick = element.target.name == "todoCheck";
